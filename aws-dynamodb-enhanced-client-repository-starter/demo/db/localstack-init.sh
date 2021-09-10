9226e2183602ad292d52fb5106371c1b2cc7eb4a
bhuwan.upadhyay49@gmail.com <bhuwan.upadhyay49@gmail.com>
2021-09-10 17:50:38 +0545

    fix: artifacts

--- aws-dynamodb-enhanced-client-repository-starter/demo/db/localstack-init.sh
+++ aws-dynamodb-enhanced-client-repository-starter/demo/db/localstack-init.sh
@@ -6,6 +6,6 @@ awslocal s3 mb s3://file-store
 
 awslocal dynamodb create-table \
    --table-name Payments \
-   --attribute-definitions AttributeName=Reference,AttributeType=S \
-   --key-schema AttributeName=Reference,KeyType=HASH \
+   --attribute-definitions AttributeName=Id,AttributeType=S \
+   --key-schema AttributeName=Id,KeyType=HASH \
    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
